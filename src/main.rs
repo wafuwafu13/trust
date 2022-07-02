3eff97a8e2184c5280e731ee59ca35983d3f2fd3
wafuwafu13 <mariobaske@i.softbank.jp>
2022-07-02 17:28:56 +0900

    48:03

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,25 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        if proto != 0x0800 {
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]);
+        if eth_proto != 0x0800 {
             // no ipv4
             continue;
         }
 
         match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
             Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
                 eprintln!(
-                    "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
-                    nbytes - 4,
-                    flags,
-                    proto,
-                    p
-                );
+                    "{} -> {} {}b of protocol {:x}",
+                    src,
+                    dst,
+                    p.payload_len(),
+                    proto
+                )
             }
             Err(e) => {
                 eprintln!("ignoring weird packet {:?}", e)
