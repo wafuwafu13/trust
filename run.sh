78f3ac1edbb978f693745642d495d2834c8d2d59
wafuwafu13 <mariobaske@i.softbank.jp>
2022-07-02 16:45:32 +0900

    39:50

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ sudo setcap cap_net_admin=eip ~/desktop/trust/target/release/trust
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,15 @@ fn main() -> io::Result<()> {
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        eprintln!(
+            "read {} bytes (flags: {:x}, proto: {:x}): {:x?}",
+            nbytes - 4,
+            flags,
+            proto,
+            &buf[4..nbytes]
+        );
     }
     Ok(())
 }
